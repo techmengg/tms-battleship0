f43728e1c6dcd92d693e9cc170113da709d45995
Techmeng Aing <aingtechmeng@gmail.com>
2024-04-19 13:36:49 -0400

    created .java files for its respected class

---
+++ Board.java
@@ -0,0 +1,29 @@
+public class Board{
+
+    private String[][] squares;
+  
+    public Board(){
+  
+    }
+  
+    public String toString(){
+      return "";
+    }
+  
+    public boolean addShip(int row, int col, int len, boolean horizontal){
+      return false;
+    }
+  
+    public boolean foundShip(int len){
+      return false;
+    }
+  
+    public int shoot(int row, int col){
+      return 0;
+    }
+  
+    public boolean gameOver(){
+      return false;
+    }
+  
+  }
